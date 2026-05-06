bf91e3072885230457bcc890383cbb9a3056a33d
Alperen Araz <alperenaraz@Alperen-MacBook-Air.local>
2026-05-06 16:45:03 +0200

    Initial project setup

---
+++ test_logic.py
@@ -0,0 +1,13 @@
+import unittest
+from geometry import triangle_area, square_area
+
+class TestGeometry(unittest.TestCase):
+
+    def test_triangle(self):
+        self.assertEqual(triangle_area(10, 5), 25.0)
+
+    def test_square(self):
+        self.assertEqual(square_area(4), 16)
+
+if __name__ == "__main__":
+    unittest.main()
